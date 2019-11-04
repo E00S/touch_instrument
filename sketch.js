7717ba2f8cad6b90c7e21207c7b2e4fa1e607f9a
E00S <leonrogge@lclark.edu>
2019-11-04 03:06:49 -0800

    touches test 2

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var fft;
 let noise
 let noise_env
 
+var bool;
+
 let frequency = 50
 
 let circle_radius = 0
@@ -14,8 +16,11 @@ let circle_y = 0
 
 var volhistory = [];
 
+var note;
 var num_bars = 128;
 var w;
+
+let reverb;
 
 
 function setup() {
@@ -23,27 +28,16 @@ function setup() {
     let canvas = createCanvas(windowWidth, windowHeight)
     canvas.parent("p5")
 
-    // create new oscillator 
-    osc = new p5.Oscillator()
-    osc.setType("sine") // "sine" "square" "sawtooth"
-    osc.amp(0)  // set initial amplitude to 0
+    osc = new p5.TriOsc();
+    osc.start();
+    osc.amp(0);
 
-    // create new envelope
-    env = new p5.Envelope()
-    env.setADSR(0.2, 0.1, 0.1, 0.5)
-
-
-    // create new noise maker
-    noise = new p5.Noise()
-    noise.setType("white") // "brown" "pink"
-    noise.amp(0)  // set initial amplitude to 0
-
-    noise_env = new p5.Envelope()
-    noise_env.setADSR(0.01, 0.1, 0, 0)
-
-    fft = new p5.FFT(.9, num_bars);
+    fft = new p5.FFT(.89, num_bars);
     w = width/num_bars;
+    bool = false;
 
+    reverb = new p5.Reverb();
+    reverb.connect();
 }
 
 
@@ -57,11 +51,29 @@ function draw() {
     var spec = fft.analyze();
     for (var i = 0; i < spec.length; i++) {
         var amp = spec[i];
-        var y = map(amp, 0, 256, height, 0);
+        var y = map(amp, 0, 280, height, 0);
         line(i * w, height, i * w, y);
     }
     if(touches.length >= 1) {
         print(touches);
+    }
+
+    strokeWeight(2);
+    noFill();
+    if(circle_radius > 0) {
+        ellipse(circle_x, circle_y, circle_radius - 0, circle_radius - 0);
+        ellipse(circle_x, circle_y, circle_radius - 20, circle_radius - 20);
+        ellipse(circle_x, circle_y, circle_radius - 40, circle_radius - 40);
+    }
+
+    if(bool && circle_radius < 100) {
+        circle_radius += 5;
+    }
+    if(circle_radius >= 100) {
+        bool = false;
+    }
+    if (circle_radius > -1 && bool == false) {
+        circle_radius -= 5;
     }
 }
 
@@ -75,16 +87,14 @@ function mousePressed() {
 
     // trigger the osc envelope
     osc.start()
-    osc.amp(env)
-    env.triggerAttack()
 
-    // trigger the noise envelope
-    noise.start()
-    noise.amp(noise_env)
-    noise_env.triggerAttack()
+    bool = true;
+    
+    // Fade it in
+    osc.fade(1,.3);
 
-    // set the circle to the mouse position and increase its radius
-    circle_radius = 100
+    circle_radius = 0;
+
     circle_x = mouseX
     circle_y = mouseY
 
@@ -93,19 +103,30 @@ function mousePressed() {
 
 
 function mouseReleased() {
-
-    // "release" the envelopes
-    env.triggerRelease()
-    noise_env.triggerRelease()
-
+    osc.fade(0,0.3);
+    // bool = false;
 }
 
 
 function mouseDragged() {
 
     // map the mouse position to a frequency variable
-    frequency = map(mouseY, 0, height, 2000, 50)
-    osc.freq(frequency) // set the osc to this frequency
+    if(touches == 1){
+        note = map(mouseY, 0, height, 40, 80);
+        osc.freq(midiToFreq(Math.floor(note)));
+    }
+    if(touches == 2){
+        let reverb_time = map(mouseY, 0, height, .1, 3);
+        reverb.process(osc, reverb_time, 2);
+    }
+    if(touches == 3){
+        note = map(mouseY, 0, height, 40, 80);
+        osc.freq(midiToFreq(Math.floor(note)));
+    }
+    if(touches == 4){
+        note = map(mouseY, 0, height, 40, 80);
+        osc.freq(midiToFreq(Math.floor(note)));
+    }
 
     // track the mouse position with circle variables
     circle_x = mouseX
